16b0d705b777aa7c0a0eb10572d22646e9affbc4
syborg101 <synishigata101@yahoo.com>
2015-10-06 06:05:58 -1000

    ic 12

---
+++ client/app.js
@@ -0,0 +1,20 @@
+Template.app.helpers({
+    //add you helpers here
+});
+
+Template.app.events({
+    //add your events here
+});
+
+Template.app.onCreated(function () {
+    //add your statement here
+});
+
+Template.app.onRendered(function () {
+    //add your statement here
+});
+
+Template.app.onDestroyed(function () {
+    //add your statement here
+});
+
---
+++ client/input.js
@@ -0,0 +1,20 @@
+Template.input.helpers({
+    //add you helpers here
+});
+
+Template.input.events({
+    //add your events here
+});
+
+Template.input.onCreated(function () {
+    //add your statement here
+});
+
+Template.input.onRendered(function () {
+    //add your statement here
+});
+
+Template.input.onDestroyed(function () {
+    //add your statement here
+});
+
